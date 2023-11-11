fea7a26ce814c45f35c9c3c2ea2758977ceedaf8
Ubuntu <ubuntu@ip-172-31-11-170.ap-northeast-1.compute.internal>
2023-11-11 16:23:54 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_06_144530) do
+ActiveRecord::Schema.define(version: 2023_11_11_160441) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
